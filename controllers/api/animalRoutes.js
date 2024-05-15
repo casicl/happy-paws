3e70c5fb8112341ad1280c30b8e503a9d054b819
jeflynn135 <jenna.flynn135@gmail.com>
2024-05-14 20:39:02 -0700

    resolved merges

--- controllers/api/animalRoutes.js
+++ controllers/api/animalRoutes.js
@@ -2,6 +2,8 @@ const router = require('express').Router();
 const { Model } = require('sequelize');
 const { animals } = require('../../models');
 const withAuth = require('../../utils/auth');
+const cloudinary = require('../../utils/cloudinary');
+const upload = require('../../utils/multer');
 
 router.post('/', withAuth, async (req, res) => {
     try {
@@ -34,5 +36,28 @@ router.delete('/:id', withAuth, async (req, res) => {
         res.status(500).json(err);
     }
 });
+
+
+router.put('/images', upload.single('imageUser'), async (req, res) => {
+
+  try {
+
+    const result = await cloudinary.uploader.upload(req.file.path)
+    animals.update({
+      imagename: req.file.originalname,
+      animal_image: result.secure_url,
+      cloudinary_id: result.public_id,
+    }, {
+      where: {
+        id: req.session.user_id
+      }
+    }).then(answer => {
+      res.json(answer)
+    })
+  } catch (err) {
+    console.log(err)
+  }
+})
+
 
 module.exports = router;
--- public/css/public/js/images.js
+++ public/css/public/js/images.js
@@ -3,7 +3,7 @@ async function uploadImage(event) {
     event.preventDefault();
     const imageData = document.getElementById("image-save")
   
-    let response = await fetch('/api/users/images', {
+    let response = await fetch('/api/animals/images', {
       method: 'PUT',
       body: new FormData(imageData)
     });
