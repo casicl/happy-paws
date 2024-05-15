03590803f2e0dc53198a69ee43eabc73e6fd7919
CrackerJuice <Alex.marie3976@gmail.com>
2024-05-14 20:23:39 -0700

    Attempts to make gitignore file to work

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,3 +2,13 @@ const router = require("express").Router();
 const  {} = require("../models");
 const withAuth = require("../utils/auth");
 
+router.get('/',withAuth, async (req, res)  => {
+    try {
+        consData = await User.findAll ({
+            attributes: { exclude: ['password']},
+            order ['name', 'ASC'],
+        });
+
+        const users= userData.map((project) => project.get({plain: true}));
+})
+};
