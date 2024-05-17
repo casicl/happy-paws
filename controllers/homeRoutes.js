42ec0e010b53365b5e9cf65ae6a10f438643dbe8
Casi Clarkson <casiclarkson@gmail.com>
2024-05-16 18:52:44 -0700

    code added to homeroutes, id tags in handlebars pages

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,10 @@
 const router = require("express").Router();
 const  {} = require("../models");
 const withAuth = require("../utils/auth");
+const Animal = require("../models/animals");
 
+router.get("/", async (req, res)=> {
+    const animals = await Animal.findAll({where: {user_id: {[Op.is]:null}}})
+    
+})
 module.exports = router;
