7a6c32091eac7276d2a17ee350149021296a2aee
jeflynn135 <jenna.flynn135@gmail.com>
2024-05-19 22:27:52 -0700

    added helpers

---
+++ utils/helpers.js
@@ -0,0 +1,18 @@
+module.exports = {
+    // format_time: (date) => {
+    //     return date.toLocaleTimeString();
+    // },
+    format_date: (date) => {
+        return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${
+            new Date(date).getFullYear()
+        }`;
+    },
+    format_url: (url) => {
+        return url
+        .replace('http://', '')
+        .replace('https://', '')
+        .replace('www.', '')
+        .split('/')[0]
+        .split('?')[0];
+    },
+};
